68bcc893c30d930285febbe8a4e27ef2179960d8
Luiss Resources <luissresources@gmail.com>
2022-07-15 16:48:56 -0500

    add states for each field

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,44 +1,124 @@
+import { useState, useEffect } from "react";
+
 const Form = () => {
+  const [petsName, setPetsName] = useState('Pet');
+  const [petOwner, setPetOwner] = useState('User');
+  const [email, setEmail] = useState('');
+  const [discharge, setDischarge] = useState('');
+  const [symtomDetail, setSymtomDetail] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Simple validation 
+    if ([petsName, petOwner, email, discharge, symtomDetail].includes('')) {
+      setError(true);
+    }else {
+      setError(false);
+    }
+  };
+
   return (
-    <div className='mb-6 px-6 md:px-0'>
-      <h2 className="px-1 py-2 text-center text-white bg-indigo-600 rounded-md">Seguimiento pacientes</h2>
+    <div className="mb-6 px-6 md:px-0">
+      <h2 className="px-1 py-2 text-center text-white bg-indigo-600 rounded-md">
+        Patient follow-up
+      </h2>
       <p className="text-center my-6">
-        Añade pacientes y{' '}
-        <span className="text-indigo-700 text capitalize">
-          administradores
-        </span>
+        Add patients y{" "}
+        <span className="text-indigo-700 text capitalize">administrators</span>
       </p>
 
-      <form action="" className="bg-white rounded-md shadow-md px-4 py-8">
+      <form 
+        action="" 
+        className="bg-white rounded-md shadow-md px-4 py-8"
+        onSubmit={handleSubmit}>
+          { error && (
+            <p 
+            className='text-white bg-red-700 py-1 px-1 my-4 uppercase text-center'
+            >
+              Complete los datos, para enviar el formulario
+            </p>
+          )}
         <div className="flex flex-col mb-4">
-          <label htmlFor="pets-name" className="uppercase text-gray-800 font-bold">Pet's name</label>
+          <label
+            htmlFor="pets-name"
+            className="uppercase text-gray-800 font-bold"
+          >
+            Pet's name
+          </label>
           <input
-           type="text" id="pets-name" placeholder="Name" className="py-1 border border-indigo-300 px-2 placeholder-gray-500"/>
+            type="text"
+            id="pets-name"
+            placeholder="Name"
+            className="py-1 border border-indigo-300 px-2 placeholder-gray-500"
+            value={petsName}
+            onChange={(e) => setPetsName(e.target.value)}
+          />
         </div>
         <div className="flex flex-col mb-4">
-          <label htmlFor="pet-owner" className="uppercase text-gray-800 font-bold">Pet owner</label>
+          <label
+            htmlFor="pet-owner"
+            className="uppercase text-gray-800 font-bold"
+          >
+            Pet owner
+          </label>
           <input
-           type="text" id="pet-owner" placeholder="Owner name" className="py-1 border border-indigo-300 px-2 placeholder-gray-500"/>
+            type="text"
+            id="pet-owner"
+            placeholder="Owner name"
+            className="py-1 border border-indigo-300 px-2 placeholder-gray-500"
+            value={petOwner}
+            onChange={(e) => setPetOwner(e.target.value)}
+          />
         </div>
         <div className="flex flex-col mb-4">
-          <label htmlFor="email" className="uppercase text-gray-800 font-bold">Email</label>
+          <label htmlFor="email" className="uppercase text-gray-800 font-bold">
+            Email
+          </label>
           <input
-           type="email" id="email" placeholder="Email" className="py-1 border border-indigo-300 px-2 placeholder-gray-500"/>
+            type="email"
+            id="email"
+            placeholder="Email"
+            className="py-1 border border-indigo-300 px-2 placeholder-gray-500"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="flex flex-col mb-4">
-          <label htmlFor="date" className="uppercase text-gray-800 font-bold">Discharge</label>
+          <label htmlFor="discharge" className="uppercase text-gray-800 font-bold">
+            Discharge
+          </label>
           <input
-           type="date" id="date" className="py-1 border border-indigo-300 px-2 placeholder-gray-500"/>
+            type="date"
+            id="discharge"
+            className="py-1 border border-indigo-300 px-2 placeholder-gray-500"
+            value={discharge}
+            onChange={(e) => setDischarge(e.target.value)}
+          />
         </div>
         <div className="flex flex-col mb-4">
-          <label htmlFor="symptom detail" className="uppercase text-gray-800 font-bold">Symtom detail</label>
+          <label
+            htmlFor="symptom detail"
+            className="uppercase text-gray-800 font-bold"
+          >
+            Symtom detail
+          </label>
           <textarea
-           id="symptom detail" className="py-1 border border-indigo-300 px-2 placeholder-gray-500"/>
+            id="symptom detail"
+            className="py-1 border border-indigo-300 px-2 placeholder-gray-500"
+            value={symtomDetail}
+            onChange={(e) => setSymtomDetail(e.target.value)}
+          />
         </div>
-        <input type="submit" value="Save" className="py-2 text-center bg-indigo-600 w-full text-white hover:bg-green-700 transition-all cursor-pointer font-bold"/>
+        <input
+          type="submit"
+          value="Save"
+          className="py-2 text-center bg-indigo-600 w-full text-white hover:bg-green-700 transition-all cursor-pointer font-bold"
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Form;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header () {
     return (
         <h2 className="text-3xl text-black font-black flex flex-col justify-center items-center md:flex-col py-2.5 text-center capitalize">
-            Sistema de seguimiento de pacientes
-            <span className="ml-1 text-indigo-600 font-black">Veterinaria</span>
+            Patient tracking system
+            <span className="ml-1 text-indigo-600 font-black">Vet</span>
         </h2>
     )
 }
